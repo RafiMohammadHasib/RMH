d771993864a6a8c0f6996afe605d8f3e5d22f3d4
Rafi Md. Hasib <rafi.euroit@gmail.com>
2025-09-14 07:14:08 +0000

    i joind here in July

--- src/lib/data.ts
+++ src/lib/data.ts
@@ -1,3 +1,4 @@
+
 export const personalInfo = {
   name: "Rafi Mohammad Hasib",
   title: "Professional Business Developer & Software Engineer",
@@ -35,7 +36,7 @@ export const skills = [
 export const experiences = [
     {
         company: "Euro IT",
-        period: "May 2025 - Present",
+        period: "July 2024 - Present",
         title: "Business Developer and Assistant Software Engineer",
         responsibilities: [
             "Talk to clients for business needs, offer services, and arrange meetings to close deals.",
@@ -49,7 +50,7 @@ export const experiences = [
     },
     {
         company: "Lnkits Technology",
-        period: "September 2024 - April 2025",
+        period: "September 2023 - April 2024",
         title: "Business Development Executive",
         responsibilities: [
             "Met with new and current clients to understand their needs and explain software services.",
@@ -62,7 +63,7 @@ export const experiences = [
     },
     {
         company: "Softifybd Limited",
-        period: "October 2023 - August 2024",
+        period: "October 2022 - August 2023",
         title: "Sales and Marketing Specialist (Software)",
         responsibilities: [
             "Attend client business meetings to understand needs and recommend suitable software solutions.",
@@ -76,18 +77,18 @@ export const experiences = [
     },
     {
         company: "INovex Idea Solution Limited",
-        period: "May 2023 - October 2023",
+        period: "May 2022 - October 2022",
         title: "Sales and Marketing Executive (Software)",
         responsibilities: [
             "Created and posted content on social media to promote company services.",
-            "Tested mobile apps to make sure they worked well before launch",
-            "Attended meetings to learn about client needs and offer the right products",
+            "Tested mobile apps to make sure they worked well before launch.",
+            "Attended meetings to learn about client needs and offer the right products.",
             "Built strong connections with clients to earn their trust and grow business."
         ]
     },
     {
         company: "Digital Marketing Solution (Pvt.) Limited",
-        period: "November 2022 - April 2023",
+        period: "November 2021 - April 2022",
         title: "Marketing Executive",
         responsibilities: [
             "Tested websites and found areas that needed fixing or improvement.",
